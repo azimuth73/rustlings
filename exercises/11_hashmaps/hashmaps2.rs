08275a65fb46e77e5a351bae8bad9ad70af23b65
azimuth73 <playroger2018@gmail.com>
2024-07-12 20:21:18 +0200

    Finish hashmaps

--- exercises/11_hashmaps/hashmaps2.rs
+++ exercises/11_hashmaps/hashmaps2.rs
@@ -19,7 +19,7 @@ enum Fruit {
 }
 
 fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
-    let fruit_kinds = [
+    let fruit_kinds: [Fruit; 5] = [
         Fruit::Apple,
         Fruit::Banana,
         Fruit::Mango,
@@ -31,7 +31,12 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         // TODO: Insert new fruits if they are not already present in the
         // basket. Note that you are not allowed to put any type of fruit that's
         // already present!
+        basket.entry(fruit).or_insert(1);
     }
+
+    
+
+    
 }
 
 fn main() {
